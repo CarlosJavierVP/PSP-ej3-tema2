3cd52dffd99260d2c8fea327f1e9d68144b7cdcc
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-03 08:25:51 +0200

    primer commit

---
+++ src/main/java/Ejercicio3.java
@@ -0,0 +1,16 @@
+
+
+
+public class Ejercicio3 implements Runnable{
+
+    VariableCompartida vc;
+
+    public Ejercicio3(VariableCompartida vc) {
+        this.vc = vc;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/VariableCompartida.java
@@ -0,0 +1,21 @@
+
+
+public class VariableCompartida{
+
+    private int v;
+
+    public VariableCompartida(int v) {
+        this.v = v;
+    }
+
+    public int getV() {
+        return v;
+    }
+
+    public void setV(int v) {
+        this.v = v;
+    }
+
+
+
+}
