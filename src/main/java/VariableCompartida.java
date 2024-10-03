331e3c9178b2959c652e3681ba1d30fbd96f36fb
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-03 09:56:35 +0200

    Commit corregido errores en el get y set

--- src/main/java/VariableCompartida.java
+++ src/main/java/VariableCompartida.java
@@ -9,6 +9,15 @@ public class VariableCompartida{
         this.v = v;
     }
 
+    /*
+    * Antes de llamar a los métodos wait() y notify() únicamente la hebra consumidora devolvía
+    * el último resultado(99) ya que los hilos se ejecutaban desordenados. Una vez que se han sincronizado
+    * el get(utilizado por la HebraProductora) y set(utilizado por la HebraConsumidora) y utilizado los
+    * métodos wait() y notify() los resultados se han sincronizado
+    * Se utiliza una variable booleana de cara a controlar el método wait()
+    *
+    */
+
     public synchronized int getV() {
         while (!flag){
             try {
